038a14cbf6f3cdf440fc676cb941e199df82e832
Alex@cyberly.2soft <Alex@cyberly.2soft>
2013-11-06 16:57:33 +0700

    Добавлено поле типа rich_editor

---
+++ addons/form_elements/rich_editor_field.php
@@ -0,0 +1,18 @@
+<?php
+
+	Application::loadLibrary('olmi/editor');
+
+	class coreFormElementsAddonRichEditorField extends TEditorField {
+		
+		
+		public function __construct($name, $params) {
+			
+			parent::__construct(
+				$name,
+				isset($params['value']) ? $params['value'] : '',
+				isset($params['width']) ? $params['width'] : 600,
+				isset($params['height']) ? $params['height'] : 300
+			);	
+		}
+		
+	}
