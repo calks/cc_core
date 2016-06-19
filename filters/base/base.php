61d05f8127fdf843cba496db392fd2bbfd909734
Alex <none@none>
2016-06-19 18:45:13 +0600

    Значение фильтра сохраняется в сессии при изменении заначения поля вызовом setValue()

--- filters/base/base.php
+++ filters/base/base.php
@@ -44,6 +44,13 @@
 			}
 
 		}
+		
+		
+		public function setValue($field_name, $value) {
+			$out = parent::setValue($field_name, $value);
+			$this->saveToSession();
+			return $out;		
+		}
 
 		protected function isSearchQueryPosted() {
 			if (!Request::isPostMethod()) return false;
