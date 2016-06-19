6b03f92ba675b9d0bf5712c7713d949da2c2ed58
Alex <none@none>
2016-06-19 16:27:06 +0600

    Добавлены поля time и datetime

---
+++ form_fields/datetime.php
@@ -0,0 +1,46 @@
+<?php
+	
+	class coreDatetimeFormField extends coreBaseFormField {
+	
+		protected $date_field;
+		protected $time_field;
+		
+		
+		public function __construct($name) {
+			parent::__construct($name);
+			$this->date_field = coreFormElementsLibrary::get('date', $name . '_date');
+			$this->time_field = coreFormElementsLibrary::get('time', $name . '_time');
+		}		
+		
+		public function getAsHtml() {
+			$at = $this->gettext(' at ');
+			return $this->date_field->getAsHtml() . $at . $this->time_field->getAsHtml();		
+		}
+		
+		
+		public function SetFromPost($POST) {
+			$this->date_field->setFromPost($POST);
+			$this->time_field->setFromPost($POST);
+			
+			$date = coreFormattingLibrary::dateRussianToMysql($this->date_field->getValue());
+			$time = $this->time_field->getValue();
+			
+			$this->value = trim("$date $time");		
+			
+		}
+		
+		
+		public function setValue($value) {
+			$parts = explode(' ', $value);
+			foreach ($parts as $p) {
+				if (strpos($p, ':') !== false) {
+					$this->time_field->setValue($p);
+				}
+				else {
+					$this->date_field->setValue(coreFormattingLibrary::dateMysqlToRussian($p));
+				}
+			}
+		}
+		
+	
+	}
